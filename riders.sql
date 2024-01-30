7cff07fb1372cfabaa504d1dcf6ed7a6fc192239
PedroLP7 <113777475+PedroLP7@users.noreply.github.com>
2024-01-30 15:53:05 +0100

    a

--- riders.sql
+++ riders.sql
@@ -8,18 +8,26 @@ CREATE TABLE user_type (
   type_id INT PRIMARY KEY auto_increment,
   type_name VARCHAR(100)
 ); 
+
 -- create user table
 CREATE TABLE user (
-    user_id INT PRIMARY KEY,
-    surname1 VARCHAR(100),
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    userName VARCHAR(100) NOT NULL ,
+    realName varchar(100),
+    surname1 VARCHAR(100) NOT NULL ,
     surname2 VARCHAR(100),
-    phone_number VARCHAR(100),
+    phone_number VARCHAR(9) UNIQUE,
     profile_img_filepath VARCHAR(100),
-    dni VARCHAR(100),
-    cif VARCHAR(100),
+    dniCif VARCHAR(9)  UNIQUE ,
     adress VARCHAR(100),
     post_code VARCHAR(100),
+    pswrd VARCHAR(100) NOT NULL ,
     user_type_id int,
+    isActive boolean,
+    isCanceled boolean,
+    size int ,
+    location varchar (100),
+    fed boolean,
     -- add user type foreign key
 	FOREIGN KEY (user_type_id)
     REFERENCES user_type(type_id)
@@ -32,22 +40,18 @@ CREATE TABLE order_status (
 -- create order content table
 CREATE TABLE charity_menu (
 id_menu INT PRIMARY KEY auto_increment,
- items VARCHAR(100)
- ); 
- CREATE TABLE customer (
-    id_customer int PRIMARY KEY auto_increment,
-    location varchar (150),
-    quantity int,
-    nickname varchar (50),
-    customer_type_id int,
-    FOREIGN KEY (customer_type_id)
-    REFERENCES user_type(type_id)
-); 
+ item1 varchar(100),
+ item2 varchar(100),
+ item3 varchar(100)
+ ); -- create customerType
+
+
+
 -- create order table
 CREATE TABLE food_order (
  order_id INT PRIMARY KEY auto_increment,
  charity_menu_id INT,
- date VARCHAR(100),
+ date VARCHAR(100) NOT NULL,
  id_provider INT,
  id_rider INT,
  id_customer INT,
@@ -60,7 +64,7 @@ CREATE TABLE food_order (
     REFERENCES user(user_id),
       -- add customer foreign key
     FOREIGN KEY (id_customer)
-    REFERENCES customer (id_customer),
+    REFERENCES user (user_id),
       -- add order status foreign key
     FOREIGN KEY (id_order_status)
     REFERENCES order_status(status_id),
